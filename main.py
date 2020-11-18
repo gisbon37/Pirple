4bc9a4872e1502c20c8e875143249815e8cb7b12
gisbon37 <38424222+gisbon37@users.noreply.github.com>
2020-11-18 12:25:54 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+Homework Assignment #1: Variables
+
+What's your favourite song?
+
+Coder:  Terry Blow
+
+"""
+
+# Favourite Song & Author
+FaveTrack = "Wild West Hero"
+B_Side = "Eldorado"
+Artist = "Electric Light Orchestra"
+HearSay = "The legend is that the song's lyrics were written in four minutes"
+
+# Band Information
+Album = "Out of the Blue"
+Artist = "Electric Light Orchestra"
+Genre = "Progressive Rock"
+B_Side = "Eldorado"
+
+# Song Statistics
+RelYear = 1978
+Length = 4.41
+ChartPosUK = 6
+
+print (FaveTrack)
+print (B_Side)
+print (Length)
+print (ChartPosUK)
+print (Album)
+print (Artist)
+print (RelYear)
+print (HearSay)
